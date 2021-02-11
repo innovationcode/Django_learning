8ce1aa685bbb66d22a40c7992a2506cde9cb4d77
lajawanti <lajawanti.dhake@gmail.com>
2021-02-11 16:08:49 -0500

    changes with templates

--- hello_django/hello/app1/views.py
+++ hello_django/hello/app1/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-      return HttpResponse("<h1 style = 'color: red'>Hello....<h1>")
+      # return HttpResponse("<h1 style = 'color: red'>Hello....<h1>")
+      return render(request, 'basic.html', {'title' : 'Django'})
 
 def profile(request):
       return HttpResponse("My profile page....")
